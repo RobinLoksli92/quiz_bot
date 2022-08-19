d5e4273f41db6732f40bf2a0bd23a6386d82b12f
RobinLoksli92 <gleb11tiun@mail.ru>
2022-08-20 00:55:36 +0200

    Get questions from all files in folder

--- get_questions.py
+++ get_questions.py
@@ -1,19 +1,24 @@
+import os
+
+
 def get_questions():
     questions = []
-    with open('questions/znvd98.txt', 'r', encoding='KOI8-R') as file:
-        questions = file.read().split('\n\n')
+    for filename in os.listdir('questions'):
+        with open(f'questions/{filename}', 'r', encoding='KOI8-R') as file:
+            questions += file.read().split('\n\n')
 
-    questions_and_answers = {}
-    for text_part in questions:
-        if 'Вопрос' in text_part:
-            questions_and_answers[text_part] = questions[questions.index(text_part)+1]
+        questions_and_answers = {}
+        for text_part in questions:
+            if 'Вопрос' in text_part:
+                questions_and_answers[text_part] = questions[questions.index(text_part)+1]
 
-    return questions_and_answers
+        return questions_and_answers
 
 
 def main():
     quest = get_questions()
     print(quest)
+
 
 if __name__ == '__main__':
     main()
